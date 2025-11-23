d8d4acab5d581d9c38eaedc5ba6c7d8b467cfe2f
rtlavkush07 <rtlavkush07@gmail.com>
2025-11-23 09:04:42 +0530

    added deployed backend api  url to the frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Upload from "./components/Upload";
 
 export default function App() {
-  const backendUrl = "http://localhost:5000/api"; // backend route
+  const backendUrl = import.meta.env.VITE_BACKEND_URL + "/api"; // backend route
+  
   return (
     <div>
-      
       <Upload backendUrl={backendUrl} />
     </div>
   );
 }
+
